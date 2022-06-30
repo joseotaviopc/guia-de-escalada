c94a91cdf6194a04990cb94714a7138482d5eec1
José Otavio Carvalho <jose.carvalho@paipe.co>
2022-06-29 21:14:21 -0300

    config apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from './Router';
-
-const client = new ApolloClient({
-	uri: 'https://api-sa-east-1.graphcms.com/v2/cl4vs11fg1u0c01ud2d2q83c1/master',
-	cache: new InMemoryCache(),
-});
+import { client } from './lib/apollo';
 
 function App() {
 	return (
