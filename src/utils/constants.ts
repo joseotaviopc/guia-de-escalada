c496e52c2fe2f7e25aef2f4ff7c1ef406d06397d
José Otavio Carvalho <jose.carvalho@paipe.co>
2022-07-09 18:55:07 -0300

    add axios bcryptjs jwt iron-session & utils files

---
+++ src/utils/constants.ts
@@ -0,0 +1,13 @@
+export const API_ROUTES = {
+	SIGN_UP: '/services/auth/signup',
+	SIGN_IN: '/services/auth/signin',
+	SIGN_OUT: '/services/auth/signout',
+	GET_USER: '/services/auth/getuser',
+};
+
+export const APP_ROUTES = {
+	SIGN_UP: '/signup',
+	SIGN_IN: '/signin',
+	HOME: '/',
+	PROFILE: '/profile',
+};
